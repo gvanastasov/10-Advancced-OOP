aa8587cb0a3b83c707cf512c2e49ecdbeddee844
gva <gvanastasov@abv.bg>
2017-08-07 23:33:01 +0200

    ex 3 done

---
+++ InterfacesAndAbstraction/Ferrari/Ferrari.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Ferrari : ICar
+{
+    public Ferrari(string driver)
+    {
+        this.Driver = driver;
+    }
+
+    public string Model { get { return "488-Spider"; } }
+    public string Driver { get; private set; }
+
+    public string UseBrakes()
+    {
+        return "Brakes!";
+    }
+
+    public string PushTheGasPedal()
+    {
+        return "Zadu6avam sA!";
+    }
+
+    public override string ToString()
+    {
+        return $"{Model}/{UseBrakes()}/{PushTheGasPedal()}/{Driver}";
+    }
+}
---
+++ InterfacesAndAbstraction/Ferrari/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class StartUp
+{
+    static void Main()
+    {
+        string ferrariName = typeof(Ferrari).Name;
+        string iCarInterfaceName = typeof(ICar).Name;
+
+        bool isCreated = typeof(ICar).IsInterface;
+        if (!isCreated)
+        {
+            throw new Exception("No interface ICar was created");
+        }
+
+        var name = Console.ReadLine();
+
+        ICar car = new Ferrari(name);
+        Console.WriteLine(car.ToString());
+    }
+}
