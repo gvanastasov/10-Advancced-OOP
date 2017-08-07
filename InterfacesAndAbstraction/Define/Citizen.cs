d1aff60899126638b3a58aa316e76f6f45d41ba4
gva <gvanastasov@abv.bg>
2017-08-07 23:32:56 +0200

    ex 2 done

---
+++ InterfacesAndAbstraction/Define/Citizen.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Citizen : IPerson, IIdentifiable, IBirthable
+{
+    public Citizen(string name, int age, string id, string birthdate)
+    {
+        this.Name = name;
+        this.Age = age;
+        this.Id = id;
+        this.Birthdate = birthdate;
+    }
+
+    private int age;
+    public int Age
+    {
+        get
+        {
+            return this.age;
+        }
+
+        set
+        {
+            this.age = value;
+        }
+    }
+
+    private string name;
+    public string Name
+    {
+        get
+        {
+            return this.name;
+        }
+
+        set
+        {
+            this.name = value;
+        }
+    }
+
+    private string id;
+    public string Id
+    {
+        get
+        {
+            return this.id;
+        }
+        set
+        {
+            this.id = value;
+        }
+    }
+
+    public string Birthdate
+    {
+        get;
+        set;
+    }
+}
