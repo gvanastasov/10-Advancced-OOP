5674b3ab69936eeb477879d3cf532c9e2e88fb4e
gva <gvanastasov@abv.bg>
2017-08-07 22:12:00 +0200

    reflections lab

---
+++ Reflections/Reflections/Spy.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Text;
+using System.Reflection;
+using System.Linq;
+
+public class Spy
+{
+    public string StealFieldInfo(string className, params string[] fields)
+    {
+        var type = Type.GetType(className, false, true);
+        var instanace = Activator.CreateInstance(type);
+
+        var members = type.GetFields(BindingFlags.Instance | BindingFlags.Public | BindingFlags.NonPublic | BindingFlags.Static);
+
+        var sb = new StringBuilder();
+        sb.AppendLine($"Class under investigation: {className}");
+
+        foreach (var member in members.Where(x => fields.Contains(x.Name)))
+        {
+            sb.AppendLine($"{member.Name} = {member.GetValue(instanace)}");
+        }
+
+        return sb.ToString().Trim();
+    }
+
+    public string AnalyzeAcessModifiers(string className)
+    {
+        var sb = new StringBuilder();
+
+        var type = Type.GetType(className);
+        var fields = type.GetFields(BindingFlags.Instance | BindingFlags.Static | BindingFlags.Public);
+        var publicMethods = type.GetMethods(BindingFlags.Instance | BindingFlags.Public);
+        var nonPublicMethods = type.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic);
+
+        foreach (var field in fields)
+        {
+            sb.AppendLine($"{field.Name} must be private!");
+        }
+
+        foreach (var method in nonPublicMethods.Where(x => x.Name.StartsWith("get")))
+        {
+            sb.AppendLine($"{method.Name} have to be public");
+        }
+
+        foreach (var method in publicMethods.Where(x => x.Name.StartsWith("set")))
+        {
+            sb.AppendLine($"{method.Name} have to be private!");
+        }
+
+        return sb.ToString().Trim();
+    }
+
+    public string RevealPrivateMethods(string className)
+    {
+        var type = Type.GetType(className);
+        var methods = type.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic); 
+
+        var sb = new StringBuilder();
+        sb.AppendLine($"All Private Methods of Class: {className}");
+        sb.AppendLine($"Base Class: {type.BaseType.Name}");
+
+        foreach (var method in methods)
+        {
+            sb.AppendLine(method.Name);
+        }
+
+        return sb.ToString().Trim();
+    }
+
+    public string CollectGettersAndSetters(string className)
+    {
+        var sb = new StringBuilder();
+
+        var type = Type.GetType(className);
+        var methods = type.GetMethods(BindingFlags.Instance | BindingFlags.NonPublic | BindingFlags.Public);
+
+        foreach (var method in methods.Where(x => x.Name.StartsWith("get")))
+        {
+            sb.AppendLine($"{method.Name} will return {method.ReturnType}");
+        }
+
+        foreach (var method in methods.Where(x => x.Name.StartsWith("set")))
+        {
+            sb.AppendLine($"{method.Name} will set field of {method.GetParameters().First().ParameterType}");
+        }
+
+        return sb.ToString().Trim();
+    }
+
+}
---
+++ Reflections/Reflections/Startup.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reflections
+{
+    class Startup
+    {
+        static void Main()
+        {
+            Spy spy = new Spy();
+
+            //string result = spy.StealFieldInfo("Hacker", "username", "password");
+            //string result = spy.AnalyzeAcessModifiers("Hacker");
+            //string result = spy.RevealPrivateMethods("Hacker");
+            string result = spy.CollectGettersAndSetters("Hacker");
+
+            Console.WriteLine(result);
+        }
+    }
+}
