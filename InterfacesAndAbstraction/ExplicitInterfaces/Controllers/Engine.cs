b0ed3e8670782f1b23c2bf5e449ff33185ab842a
gva <gvanastasov@abv.bg>
2017-08-12 17:27:50 +0200

    task 10 done

---
+++ InterfacesAndAbstraction/ExplicitInterfaces/Controllers/Engine.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Engine
+{
+    public Engine()
+    {
+        this.output = new StringBuilder();
+    }
+
+    private StringBuilder output;
+
+    public void Run()
+    {
+        while (true)
+        {
+            var cmd = Console.ReadLine();
+
+            if(cmd.ToLower() == "end")
+            {
+                break;
+            }
+
+            Execute(cmd);
+        }
+
+        Console.WriteLine(output.ToString().Trim());
+    }
+
+    private void Execute(string cmd)
+    {
+        var tokens = cmd.Split(new string[] { " " }, StringSplitOptions.RemoveEmptyEntries);
+
+        var citizen = new Citizen(tokens[0], tokens[1], int.Parse(tokens[2]));
+
+        output.AppendLine(((IPerson)citizen).GetName());
+        output.AppendLine(((IResident)citizen).GetName());
+    }
+}
---
+++ InterfacesAndAbstraction/ExplicitInterfaces/Models/Citizen.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Citizen : IPerson, IResident
+{
+    public Citizen(string name, string country, int age)
+    {
+        this.name = name;
+        this.country = country;
+        this.age = age;
+    }
+
+    private int age;
+    public int Age
+    {
+        get
+        {
+            return this.age;
+        }
+    }
+
+    private string country;
+    public string Country
+    {
+        get
+        {
+            return this.country;
+        }
+    }
+
+    private string name;
+    public string Name
+    {
+        get
+        {
+            return this.name;
+        }
+    }
+
+    string IPerson.GetName()
+    {
+        return this.Name;
+    }
+
+    string IResident.GetName()
+    {
+        return $"Mr/Ms/Mrs {this.Name}";
+    }
+}
