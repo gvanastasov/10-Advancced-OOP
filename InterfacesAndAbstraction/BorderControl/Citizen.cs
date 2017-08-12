d04e7bbf09cf51cd905cb0048ea7b5dfdda5af72
gva <gvanastasov@abv.bg>
2017-08-12 17:27:28 +0200

    task 5 done

---
+++ InterfacesAndAbstraction/BorderControl/Citizen.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Citizen : IIdentifiable
+{
+    public Citizen(string name, int age, string id)
+    {
+        this.Name = name;
+        this.Age = age;
+        this.id = id;
+    }
+
+    private int age;
+    public int Age
+    {
+        get
+        {
+            return this.age;
+        }
+        set
+        {
+            this.age = value;
+        }
+    }
+
+    private string name;
+    public string Name
+    {
+        get
+        {
+            return this.name;
+        }
+        set
+        {
+            this.name = value;
+        }
+    }
+
+    private string id;
+    public string Id
+    {
+        get
+        {
+            return this.id;
+        }
+    }
+}
---
+++ InterfacesAndAbstraction/BorderControl/StartUp.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+ public class Program
+{
+    private static List<IIdentifiable> inhabitants = new List<IIdentifiable>();
+
+    static void Main()
+    {
+        while (true)
+        {
+            var cmd = Console.ReadLine();
+
+            if(cmd == "End")
+            {
+                break;
+            }
+
+            var tokens = cmd.Split();
+
+            IIdentifiable inhabitant;
+            if(tokens.Length == 2)
+            {
+                inhabitant = new Robot(tokens[0], tokens[1]);
+            }
+            else
+            {
+                inhabitant = new Citizen(tokens[0], int.Parse(tokens[1]), tokens[2]);
+            }
+            inhabitants.Add(inhabitant);
+        }
+
+        var controlNumber = Console.ReadLine();
+
+        foreach (var inhabitant in inhabitants)
+        {
+            if(inhabitant.Id.EndsWith(controlNumber))
+            {
+                Console.WriteLine(inhabitant.Id);
+            }
+        }
+    }
+}
